70ceb5dd14c691e950540ceea543294ad2851a6a
anna <alittlevegemite@gmail.com>
2025-04-11 16:25:06 +0900

    マイグレーションファイルusers_tableまで作成

--- src/database/migrations/2025_04_11_154634_create_users_table.php
+++ src/database/migrations/2025_04_11_154634_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOldUsersTable extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateOldUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('old_users', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
